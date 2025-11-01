b94137afc35c3d011ae194f6d950ccf77851861f
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-11-01 16:18:48 -0400

    Added controllers

---
+++ controllers/categoryController.mjs
@@ -0,0 +1,15 @@
+﻿import * as categoryModel from '../models/category.mjs';
+export async function getCategories(req,res){
+    let categories = await categoryModel.getAllCategories();
+    return res.status(200).json(categories);
+}
+export async function getAllInCategory(req,res){
+    if(!req.params.category){
+        return res.status(400).text("Missing Category");
+    }
+    let allInCategory = await categoryModel.getAllJokesInCategory(req.params.category);
+    if(allInCategory === null){
+        return res.status(404).json({message:"Category Not Found"});
+    }
+    return res.status(200).json(allInCategory);
+}
---
+++ controllers/jokeController.mjs
@@ -0,0 +1,25 @@
+﻿import * as jokeModel from "../models/joke.mjs";
+import * as categoryModel from '../models/category.mjs'
+export async function addJoke(req, res) {
+    let category = req.body.category;
+    if (typeof category !== "string") {
+        return res.status(400).json({message: "'category' missing or invalid"});
+    }
+    let setup = req.body.setup;
+    if (typeof setup !== "string") {
+        return res.status(400).json({message: "'setup' missing or invalid"});
+    }
+    let delivery = req.body.delivery;
+    if (typeof delivery !== "string") {
+        return res.status(400).json({message: "'delivery' missing or invalid"});
+    }
+    await jokeModel.createJoke(category, setup, delivery);
+    if (req.query.form){
+        return res.redirect(303, `/jokeCategory.html?category=${category}`);
+    }
+    return res.json(await categoryModel.getAllJokesInCategory(category));
+}
+export async function getRandomJoke(req, res) {
+    let joke = await jokeModel.getRandomJoke();
+    return res.json(joke);
+}
