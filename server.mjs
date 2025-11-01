c11e0260638c1261a6bf8f83739f174c13c6bf7a
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-11-01 16:20:28 -0400

    Added server file

---
+++ server.mjs
@@ -0,0 +1,13 @@
+﻿import express from "express";
+import categoryRoutes from "./routes/categoryRoutes.mjs";
+import jokeRoutes from "./routes/jokeRoutes.mjs";
+import multer from "multer";
+const app = express();
+app.use("/", express.static("public"));
+app.use(multer().none());
+app.use(express.json())
+app.use("/jokebook", categoryRoutes);
+app.use("/jokebook", jokeRoutes);
+app.listen(3000);
+
+//C:\Users\Sasha\source\repos\csc372-assignment7\routes\categoryRoutes.mjs
